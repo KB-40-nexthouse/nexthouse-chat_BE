bfa46d8b51e90aba174463f1f93815329dce7925
이동희 <eastshine96@naver.com>
2022-08-14 19:55:13 +0900

    테스트 Merge branch 'main' of https://github.com/KB-40-nexthouse/nexthouse-chat_BE.git into main

--- src/main/java/com/demo/microservices/controller/AdsrMgntController.java
+++ src/main/java/com/demo/microservices/controller/AdsrMgntController.java
@@ -33,7 +33,7 @@ public class AdsrMgntController {
 	@Value("${garage.product.port}")
 	String productPort;
 	
-	@ApiOperation(value="전체 상담이력 가져오기")
+	@ApiOperation(value="유저 전체 상담이력 조회")
 	@CrossOrigin
 	@GetMapping(value="/adsrMsgAll/{custNo}")
 	public ResponseEntity <List<AdsrMgnt>> getselectAdsrAll(@PathVariable String custNo) { 
